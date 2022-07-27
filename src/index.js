3515ea6d00f4601771e4e923f8e55a2c7720e9cb
Thao Ha <thaoanhhaa1@gmail.com>
2022-07-27 11:07:39 +0700

    Enbling CORS

--- src/index.js
+++ src/index.js
@@ -18,4 +18,15 @@ app.use(bodyParser.json());
 app.use(route);
 app.use(errorMiddlewares);
 
+// Enabling CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization',
+    );
+    next();
+});
+
 app.listen(port, () => console.log('App running on port ' + port));
