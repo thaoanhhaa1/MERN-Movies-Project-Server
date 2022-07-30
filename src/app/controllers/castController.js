d76ca77cd44d1d7d0fd8a7945f242c07006a29d3
Thao Ha <thaoanhhaa1@gmail.com>
2022-07-30 20:56:25 +0700

    List Movies Of Cast - Pagination

--- src/app/controllers/castController.js
+++ src/app/controllers/castController.js
@@ -4,12 +4,25 @@ module.exports = {
     // [GET] /cast/:castId/movies
     movies: async (req, res, next) => {
         const castId = req.params.castId;
+        const page = req.query?.page ?? 1;
 
         try {
             const results = await axios.get(
                 `https://api.themoviedb.org/3/person/${castId}/movie_credits?api_key=${process.env.THE_MOVIE_DB_API_KEY}`,
             );
-            res.json(results.data);
+
+            if (!results?.data?.cast)
+                res.status(400).send({ message: 'CastID Error' });
+
+            const result = results.data.cast;
+            const castOnPage = process.env.CAST_ON_PAGE;
+
+            res.json({
+                cast: result.slice((page - 1) * castOnPage, page * castOnPage),
+                page,
+                total_pages: Math.ceil(result.length / castOnPage),
+                total_results: result.length,
+            });
         } catch (error) {
             next(error);
         }
