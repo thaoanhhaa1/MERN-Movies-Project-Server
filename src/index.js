f71b577ee2b60c277a0d43936ba569621936a652
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-09-09 07:57:02 +0700

    Deploy vercel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./config/db');
 const route = require('./route');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 // const errorMiddleware = require('./app/middlewares/errorMiddleware');
 
 const port = process.env.PORT || 5000;
@@ -15,7 +18,23 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(route);
+app.use('/api', route);
+
+// Configure swagger docs
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: 'WMovies API',
+            version: '1.0.0',
+            description: 'My API for doing cool stuff!',
+        },
+    },
+    apis: [path.join(__dirname, '/route/*.js')],
+};
+
+const swaggerSpecs = swaggerJsdoc(options);
+
+app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 // app.use(errorMiddleware);
 
 // Enabling CORS
