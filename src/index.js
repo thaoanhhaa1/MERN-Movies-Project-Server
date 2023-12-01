4b45bd28b14ab0765e650f9b7d8820f87044041a
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2023-12-01 21:26:06 +0700

    Add Swagger *

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const db = require('./config/db');
 const route = require('./route');
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const expressJSDocSwagger = require('express-jsdoc-swagger');
 // const errorMiddleware = require('./app/middlewares/errorMiddleware');
 
 const port = process.env.PORT || 5000;
@@ -22,19 +21,42 @@ app.use('/api', route);
 
 // Configure swagger docs
 const options = {
-    swaggerDefinition: {
-        info: {
-            title: 'WMovies API',
-            version: '1.0.0',
-            description: 'My API for doing cool stuff!',
+    info: {
+        version: '1.0.0',
+        title: 'Albums store',
+        license: {
+            name: 'MIT',
         },
     },
-    apis: [path.join(__dirname, '/route/*.js')],
+    security: {
+        BasicAuth: {
+            type: 'http',
+            scheme: 'basic',
+        },
+    },
+    baseDir: __dirname,
+    // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+    filesPattern: './**/*.js',
+    // URL where SwaggerUI will be rendered
+    swaggerUIPath: '/api-docs',
+    // Expose OpenAPI UI
+    exposeSwaggerUI: true,
+    // Expose Open API JSON Docs documentation in `apiDocsPath` path.
+    exposeApiDocs: false,
+    // Open API JSON Docs endpoint.
+    apiDocsPath: '/v3/api-docs',
+    // Set non-required fields as nullable by default
+    notRequiredAsNullable: false,
+    // You can customize your UI options.
+    // you can extend swagger-ui-express config. You can checkout an example of this
+    // in the `example/configuration/swaggerOptions.js`
+    swaggerUiOptions: {},
+    // multiple option in case you want more that one instance
+    multiple: true,
 };
 
-const swaggerSpecs = swaggerJsdoc(options);
+expressJSDocSwagger(app)(options);
 
-app.use('/api/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 // app.use(errorMiddleware);
 
 // Enabling CORS
--- src/route/site.js
+++ src/route/site.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const siteController = require('../app/controllers/siteController');
 
+/**
+ * A song type
+ * @typedef {object} Song
+ * @property {string} title.required - The title
+ * @property {string} artist - The artist
+ * @property {number} year - The year - double
+ */
+
+/**
+ * GET /api/v1
+ * @summary This is the summary of the endpoint
+ * @return {object} 200 - success response
+ */
 router.get('/', siteController.home);
 router.get('/search', siteController.search);
 router.get('/banner', siteController.banner);
